6470ddfa3ac73af783126e4c2221c32095f4f29b
rkttu <rkttu@xinics.com>
2015-02-27 10:22:05 +0900

    change nuspec file to tokenized version

--- System.Net.FtpClient.Async/System.Net.FtpClient.Async/Properties/AssemblyInfo.cs
+++ System.Net.FtpClient.Async/System.Net.FtpClient.Async/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다.
 // 어셈블리와 관련된 정보를 수정하려면
 // 이 특성 값을 변경하십시오.
-[assembly: AssemblyTitle("System.Net.FtpClient.Async Extension")]
-[assembly: AssemblyDescription("This assembly provides async extensions for System.Net.FtpClient library.")]
+[assembly: AssemblyTitle("System.Net.FtpClient.Async Extensions")]
+[assembly: AssemblyDescription("This component provides async extensions for System.Net.FtpClient library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("rkttu.com")]
+[assembly: AssemblyCompany("ClouDeveloper")]
 [assembly: AssemblyProduct("System.Net.FtpClient.Async")]
-[assembly: AssemblyCopyright("(c) 2014 ClouDeveloper, All rights reserved.")]
+[assembly: AssemblyCopyright("(c) ClouDeveloper, All rights reserved.")]
 [assembly: AssemblyTrademark("ClouDeveloper")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
